b4aa58a3d0a72ca4cb2e0faf3d5cd70d2ec687c7
Daria <dariasiluyanowa@gmail.com>
2023-08-28 23:02:50 +0300

    collapse

--- script/popupInfo.js
+++ script/popupInfo.js
@@ -1,13 +1,16 @@
-let wbIcon = document.querySelector(".wb-info__icon");
-let wbPopup = document.getElementById("info-popup");
-let freePopUp = document.querySelector(".delivery__return__free");
-let freeInfo = document.querySelector(".delivery__return__popup");
+const wbIcon = document.querySelector(".wb-info__icon");
+const wbPopup = document.getElementById("info-popup");
 
+/*function, при наведении на иконку показывает popup
+*@param icon - элемент для наведения курсора
+*@param popup - окно popup
+*/
 const popUpOnHover = (icon, popup) =>  {
   icon.addEventListener("mouseover", showPopup);
   icon.addEventListener("mouseout", hidePopup);
   
   function hidePopup(evt) {
+    console.log("hover")
     popup.style.display = "none";
   }
   
@@ -16,8 +19,23 @@ const popUpOnHover = (icon, popup) =>  {
   }
 }
 
+/*function, при наведении на иконку показывает popup для одинаковых элементов
+*@param container - родитель для popup и иконки
+*@param elemPopup - окно popup
+*@param elemHover - элемент для наведения курсора
+*/
+const showMultiplePopUp = (container, elemPopup, elemHover) => {
+  document.querySelectorAll(container).forEach((elem)=> {
+    let icon = elem.querySelector(elemHover);
+    let popup = elem.querySelector(elemPopup);
+    popUpOnHover(icon, popup)
+  })
+}
+
+showMultiplePopUp(".delivery__return",".delivery__return__popup", ".delivery__return__free")
+showMultiplePopUp(".old-price",".old-price__popup",".old-price--cancel")
 popUpOnHover(wbIcon,wbPopup)
-popUpOnHover(freePopUp, freeInfo)
+
 
 
 
