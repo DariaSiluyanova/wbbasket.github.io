f4824c4e106f4fa02f320d4aa6f67e46976adc68
Daria <prog-1@goodfil.com>
2023-08-30 16:04:30 +0300

    mob layout

--- script/inputValid.js
+++ script/inputValid.js
@@ -1,14 +1,36 @@
 let firstName = document.getElementById("name")
+let secondName = document.getElementById("secondName")
 let errorMessage = document.getElementById("errorName")
+let errorSecondName = document.getElementById("errorSecondName")
 const patternName = /^[A-Za-zА-Яа-яЁё]+$/
 
 /**first name validation **/
-firstName.onblur = function() {
-    let valid = patternName.test(firstName.value)
-    if(!valid) {
-       errorMessage.textContent = "Укажите имя"
-       firstName.classList.add("invalid")
-    } 
-
+function validValueName() {
+    firstName.value.split("").forEach(element => {
+        let valid = patternName.test(element)
+        if(!valid) {
+            errorMessage.textContent = "Укажите имя"
+            firstName.classList.add("invalid")
+        }
+        if(valid) {
+            errorMessage.textContent = ""
+            firstName.classList.remove("invalid")
+        }
+    });
 }
+
+function validValueSecondName() {
+    secondName.value.split("").forEach(element => {
+        let valid = patternName.test(element)
+        if(!valid) {
+            errorSecondName.textContent = "Укажите фамилию"
+            secondName.classList.add("invalid")
+        }
+        if(valid) {
+        errorSecondName.textContent = ""
+            secondName.classList.remove("invalid")
+        }
+    });
+}
+
 
--- script/modalBtn.js
+++ script/modalBtn.js
@@ -3,6 +3,8 @@ const delivery = document.querySelector(".delivery")
 const modalPay = document.querySelector(".delivery-container")
 const modalAddress = document.querySelector(".delivery-container--address")
 const payment = document.querySelector(".form__payment")
+const totalAddress = document.querySelector(".addition__date-info")
+const totalPay = document.querySelector(".form__additional-block")
 
 const modalOpenClose = (domElement, modal) => {
 /**function, добавляет слушатель на кнопку Изменить, чтобы открыть окно
@@ -20,10 +22,6 @@ const modalOpenClose = (domElement, modal) => {
         enableScroll()
     })
 }
-modalOpenClose(payment, modalAddress)
-modalOpenClose(delivery, modalPay)
-
-
 /**function, убирает автоскролл браузера к невалидному инпуту
  **/
 function disableScroll() {
@@ -40,3 +38,8 @@ function disableScroll() {
 function enableScroll() {
     window.onscroll = function() {};
 }
+
+modalOpenClose(payment, modalAddress)
+modalOpenClose(totalAddress, modalAddress)
+modalOpenClose(delivery, modalPay)
+modalOpenClose(totalPay, modalPay)
