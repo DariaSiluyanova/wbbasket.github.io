f442ca0ead752bee7500ac51523ec03a513b152b
Daria <dariasiluyanowa@gmail.com>
2023-08-30 22:08:59 +0300

    validation inputs

--- script/inputValid.js
+++ script/inputValid.js
@@ -1,36 +1,106 @@
-let firstName = document.getElementById("name")
-let secondName = document.getElementById("secondName")
-let errorMessage = document.getElementById("errorName")
-let errorSecondName = document.getElementById("errorSecondName")
+const firstName = document.getElementById("name")
+const secondName = document.getElementById("secondName")
+const email = document.getElementById("email")
+const tel = document.getElementById("phone")
+const inn = document.getElementById("inn")
+const errorMessage = document.getElementById("errorName")
+const errorSecondName = document.getElementById("errorSecondName")
+const errorEmail = document.getElementById("errorEmail")
+const errorTel = document.getElementById("errorTel")
+const errorInn = document.getElementById("errorInn")
 const patternName = /^[A-Za-zА-Яа-яЁё]+$/
+const patternEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g
+const patternTel = /^\+(?:\s*\d){11}$/g
 
-/**first name validation **/
-function validValueName() {
-    firstName.value.split("").forEach(element => {
-        let valid = patternName.test(element)
-        if(!valid) {
-            errorMessage.textContent = "Укажите имя"
-            firstName.classList.add("invalid")
+/**validation name**/
+function validation(input, error, emptyMsg, errorMsg, pattern) {
+    input.onblur = function(){
+        if(input.value.length === 0) {
+            error.textContent = emptyMsg
+            input.classList.add("invalid")
         }
+    }
+
+    input.value.split("").forEach(element => {
+        let valid = pattern.test(element)
+        if(!valid) {
+            error.textContent = errorMsg
+            input.classList.add("invalid")
+        }
+
         if(valid) {
-            errorMessage.textContent = ""
-            firstName.classList.remove("invalid")
+            error.textContent = ""
+            input.classList.remove("invalid")
         }
     });
 }
-
-function validValueSecondName() {
-    secondName.value.split("").forEach(element => {
-        let valid = patternName.test(element)
-        if(!valid) {
-            errorSecondName.textContent = "Укажите фамилию"
-            secondName.classList.add("invalid")
+/**validation email, tel**/
+function checks(input, error, emptyMsg, checkMsg, pattern) {
+    input.onblur = function(){
+        let valid = pattern.test(input.value)
+    
+        if(!valid && input.value.length === 0) {
+            error.textContent = emptyMsg
+            input.classList.add("invalid")
         }
+    
+        if(!valid && input.value.length > 0) {
+            error.textContent = checkMsg
+            input.classList.add("invalid")
+        }
+    
         if(valid) {
-        errorSecondName.textContent = ""
-            secondName.classList.remove("invalid")
+            error.textContent = ""
+            input.classList.remove("invalid")
         }
-    });
+    }
+}
+/**firstname validation **/
+let validName = () => {
+    validation(firstName, errorMessage, "Укажите имя", "Укажите имя", patternName)
 }
 
+/**secondName validation **/
+let validSecondName = () => {
+    validation(secondName, errorSecondName, "Введите фамилию", "Введите фамилию", patternName)
+}
 
+let validEmail = () => {
+    checks(email, errorEmail, "Укажите электронную почту", "Проверьте адрес электронной почты", patternEmail)
+}
+
+/**add spaces to tel */
+tel.addEventListener('keyup', function(){
+    let inputValue = this.value
+    if (inputValue.length == 2 || inputValue.length == 6 || inputValue.length == 10 || inputValue.length == 13){
+     this.value= this.value + " ";
+    }
+});
+
+/**tel validation*/
+let validTel = () => {
+    checks(tel, errorTel, "Укажите номер телефона", "Формат: +9 999 999 99 99", patternTel)
+}
+
+/**Inn validation */
+function validValueInn() {
+    const info = document.querySelector(".input-field___info")
+            
+    if(inn.value.length === 0) {
+        info.style.visibility = "hidden"
+        errorInn.textContent = "Укажите ИНН"
+        inn.classList.add("invalid")
+    }
+
+    if(inn.value.length > 0 && inn.value.length !== 14) {
+        info.style.visibility = "hidden"
+        errorInn.textContent = "Проверьте ИНН"
+        inn.classList.add("invalid")
+    }
+
+    if(inn.value.length === 14) {
+        info.style.visibility = "visible"
+        errorInn.textContent = ""
+        inn.classList.remove("invalid")
+    }   
+}
