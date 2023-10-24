39f07c0f5e16551d26f75b6a2648472a2927b296
Daria <prog-1@goodfil.com>
2023-10-24 10:27:25 +0300

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task("sass", function() {
     return gulp.src("./scss/main.scss")
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("./css/"))
 })
 
